d02f9e8a2269ccfb795869070f4f9fb8caa03f4d
Admin <ask2md@gmail.com>
2018-05-04 15:55:26 +0530

    pluralsight tuts

---
+++ Tuts/django-simpleIsBetter/myproject/boards/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Board, Topic, Post
+from django.contrib.auth.models import User
+
+# Create your views here.
+def home(request):
+    boards = Board.objects.all()
+    return render(request, 'home.html', { 'boards': boards })
+
+def board_topics(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'topics.html', { 'board': board })
+
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    if request.method == 'POST':
+        subject = request.POST['subject']
+        message = request.POST['message']
+        
+        user = User.objects.first()
+
+        topic = Topic.objects.create(
+            subject=subject,
+            board=board,
+            starter=user
+        )
+
+        post = Post.objects.create(
+            message=message,
+            topic=topic,
+            created_by=user
+        )
+        return render(request, 'new_topic.html', { 'board':board })
+
+    return render(request, 'new_topic.html', {'board': board})
